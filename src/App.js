782350723cf1cf2e24acd2f49a0291cb5ff0a238
vaisakhg0 <vaisakhg30@gmail.com>
2022-05-13 10:27:18 +0530

    search

--- src/App.js
+++ src/App.js
@@ -6,12 +6,33 @@ function App() {
   return (
     <div className="container">
     <div className="row g-3">
-        <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
-            <div className="row div col"><label for="">Username</label>
-            <input type="text" className="form-control"/></div>
-            <div className="row div col"><label for="">password</label>
-            <input type="password" name="" id="" className="form-control"/></div>
-            <div className="row div col"><button className="btn btn-success">LOGIN</button></div>
+        <div className="col col-12 sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+        <div className="row g-3">
+            <div className="col col-6 sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6"><label for="">Name</label>
+            <input className="form-control" type="text" placeholder="Name"/>
+        </div>
+            <div className="col col-12 sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6"><label for="">Rollno</label>
+            <input className="form-control" type="text" placeholder="Rollno"/></div>
+            <div className="col col-12 sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6"><label for="">Admno</label>
+            <input className="form-control" type="text" placeholder="Admno"/></div>
+            <div className="col col-12 sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6"><label for="">Mobile</label>
+            <input className="form-control" type="text" placeholder="Mobile"/></div>
+            <div className="col sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12"><label for="">College</label>
+            <input className="form-control" type="text" placeholder="College"/></div>
+            <div className="col col-12 sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6"><label for="">Username</label>
+            <input className="form-control" type="text" placeholder="Username"/></div>
+            <div className="col col-12 sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6"><label for="">Password</label>
+            <input className="form-control" type="text" placeholder="Password"/></div>
+            <div className="col col-12 sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+                <a href="register.html"><button className="btn btn-success">REGISTER</button>
+
+                </a>
+            </div>
+            <div className="col sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+                <a href="index.html"><button className="btn btn-success">BACK TO REGISTER</button></a>
+            </div>
+            <div className="col"></div>
+        </div>
         </div>
     </div>
 </div>
